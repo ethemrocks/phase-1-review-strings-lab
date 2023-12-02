445b74c179c6c0d77989d9eaa901caf309d6e7d6
Ethem Kacic <ethem3k@gmail.com>
2023-12-02 14:20:54 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your code in this file!
+
+// Define the currentUser variable
+const currentUser = 'YourUserName'; // replace 'YourUserName' with the actual user name
+
+// Define the welcomeMessage variable
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// Define the excitedWelcomeMessage variable
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+// Define the shortGreeting variable
+const firstInitial = currentUser[0];
+const shortGreeting = `Welcome, ${firstInitial}!`;
+
+// Export the variables if needed (assuming you are using CommonJS modules)
+module.exports = {
+  currentUser,
+  welcomeMessage,
+  excitedWelcomeMessage,
+  shortGreeting,
+};
